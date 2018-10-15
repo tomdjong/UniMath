f0762f524465f890758eed9b77d03138f4a4ff12
Tom de Jong <tdejong.ac@gmail.com>
2018-10-15 21:32:40 +0100

    Cleanup in the form of contexts; working term_denotes_element

--- UniMath/PartialityDominances/Partiality_and_Dominances.v
+++ UniMath/PartialityDominances/Partiality_and_Dominances.v
@@ -1,47 +1,16 @@
 Require Import UniMath.Foundations.All.
 Require Import UniMath.MoreFoundations.All.
 
-Definition selects_propositions (D : UU -> UU) : UU := ∏ X, D X -> isaprop X.
-Definition sigma_closure (D : UU -> UU) : UU := ∏ (P : UU), ∏ (Q : P -> UU), D P -> (∏ (p : P), D (Q p)) -> D(∑ (p : P), Q p).
-Definition structural_dominance : UU := ∑ (D : UU -> UU), selects_propositions D × D unit × sigma_closure D.
+Section fix_a_D_and_prop_selection.
 
-Definition structural_dominance_carrier (D : structural_dominance) : UU -> UU := pr1 D.
-Definition structural_dominance_selection (D : structural_dominance) :
-  ∏ X, structural_dominance_carrier D X -> isaprop X := pr1 (pr2 D).
+Context (D : UU -> UU).
 
-(* Definition function_on_universe_from_structural_dominance (D : structural_dominance) :
-     UU -> UU := pr1 D.
-Coercion function_on_universe_from_structural_dominance :
-  structural_dominance >-> Funclass. *)
+Definition selects_propositions : UU := ∏ X, D X -> isaprop X.
+
+Context (sel : selects_propositions).
 
 Definition lift (Y : UU) := ∑ (P : UU), isaprop P × (P -> Y).
-Definition D_lift (D : UU -> UU) (Y : UU) := ∑ (P : UU), D P × (P -> Y).
-
-Definition canonical_embedding (D : UU -> UU) (sel : selects_propositions D) {Y : UU} : D_lift D Y -> lift Y.
-Proof.
-  intro h. induction h as [P pair]. induction pair as [d g].
-  split with P. split.
-  - use sel. exact d.
-  - exact g.
-Defined.
-
-Definition tame (D : UU -> UU) (sel : selects_propositions D) {X Y : UU} : (X -> D_lift D Y) -> (X -> lift Y).
-Proof.
-  intros f x.
-  use canonical_embedding.
-  - exact D.
-  - exact sel.
-  - exact (f x).
-Defined.
-
-Definition isDis_D (D : UU -> UU) (sel : selects_propositions D) {X Y : UU} (f : X -> lift Y) : UU :=
-  ∃ (f' : X -> D_lift D Y), tame D sel f' = f.
-
-Definition Dis_D (D : UU -> UU) (sel : selects_propositions D) (X Y : UU) : UU :=
-  ∑ f : X -> lift Y, isDis_D D sel f.
-
-Definition Dis_D_carrier {D : UU -> UU} {sel : selects_propositions D} {X Y : UU} (m : Dis_D D sel X Y) : X -> lift Y :=
-  pr1 m.
+Definition D_lift (Y : UU) := ∑ (P : UU), D P × (P -> Y).
 
 Definition defined  {Y : UU} : lift Y -> hProp.
 Proof.
@@ -55,25 +24,88 @@ Proof.
   simpl. exact f.
 Defined.
 
-Definition D_pas (D : structural_dominance) : UU :=
+Definition canonical_embedding {Y : UU} : D_lift Y -> lift Y.
+Proof.
+  intro h. induction h as [P pair]. induction pair as [d g].
+  split with P. split.
+  - use sel. exact d.
+  - exact g.
+Defined.
+
+Definition tame {X Y : UU} : (X -> D_lift Y) -> (X -> lift Y).
+Proof.
+  intros f x.
+  use canonical_embedding.
+  exact (f x).
+Defined.
+
+Definition isDis_D {X Y : UU} (f : X -> lift Y) : UU :=
+  ∃ (f' : X -> D_lift Y), tame f' = f.
+
+Definition Dis_D (X Y : UU) : UU :=
+  ∑ f : X -> lift Y, isDis_D f.
+
+Definition Dis_D_carrier {X Y : UU} (m : Dis_D X Y) : X -> lift Y :=
+  pr1 m.
+
+End fix_a_D_and_prop_selection.
+
+(* Structural dominances *)
+
+Definition sigma_closure (D : UU -> UU) : UU :=
+  ∏ (P : UU), ∏ (Q : P -> UU),
+  D P -> (∏ (p : P), D (Q p)) ->
+  D(∑ (p : P), Q p).
+
+Definition structural_dominance : UU :=
+  ∑ (D : UU -> UU), selects_propositions D × D unit × sigma_closure D.
+
+Definition structural_dominance_carrier (struc_dom : structural_dominance) :
+  UU -> UU := pr1 struc_dom.
+Definition structural_dominance_selection (struc_dom : structural_dominance) :
+  selects_propositions (structural_dominance_carrier struc_dom) :=
+  pr1 (pr2 struc_dom).
+
+Section fix_a_structural_dominance.
+
+Context (struc_dom : structural_dominance).
+
+Definition pas : UU :=
   ∑ (A : UU), nonempty A ×
-    Dis_D (structural_dominance_carrier D) (structural_dominance_selection D) (A × A) A.
+  Dis_D (structural_dominance_carrier struc_dom)
+        (structural_dominance_selection struc_dom)
+        (A × A) A.
 
-Definition D_pas_carrier {D : structural_dominance} (A : D_pas D) : UU := pr1 A.
+Section fix_a_pas.
 
-Definition D_pas_app {D : structural_dominance} (A : D_pas D) : Dis_D (structural_dominance_carrier D) (structural_dominance_selection D) (D_pas_carrier A × D_pas_carrier A) (D_pas_carrier A) :=
-  pr2 (pr2 A).
+Context (A : pas).
+
+Definition pas_carrier : UU := pr1 A.
+
+Definition pas_disciplined_map :
+  Dis_D (structural_dominance_carrier struc_dom)
+        (structural_dominance_selection struc_dom)
+        (pas_carrier × pas_carrier) (pas_carrier)
+  := pr2 (pr2 A).
+
+Definition pas_app : pas_carrier × pas_carrier -> lift(pas_carrier) := pr1 pas_disciplined_map.
 
 (* Terms over a pas *)
 
-Inductive terms_over_pas {D : structural_dominance} (A : D_pas D) : UU :=
-  | var : nat -> terms_over_pas A
-  | con : D_pas_carrier A -> terms_over_pas A
-  | app : terms_over_pas A -> terms_over_pas A -> terms_over_pas A.
+Section fix_a_var_type.
 
-Inductive term_denotes_element {D : structural_dominance} {A : D_pas D} : terms_over_pas A -> D_pas_carrier A -> UU :=
-  | con_denotes : ∏ (a : D_pas_carrier A), term_denotes_element (con A a) a
-  | app_denotes : ∏ (s t : terms_over_pas A), ∏ (a b : D_pas_carrier A),
-                  ∏ (u : Dis_D_carrier (D_pas_app A) (a ,, b)),
-                  ∏ (p : defined u),
-      term_denotes_element s a -> term_denotes_element t b -> term_denotes_element (app s t) (value u p).
+Context (X : Type).
+
+Inductive terms_over_pas : UU :=
+  | var : X -> terms_over_pas
+  | con : pas_carrier -> terms_over_pas
+  | app : terms_over_pas -> terms_over_pas -> terms_over_pas.
+
+Inductive term_denotes_element : terms_over_pas -> pas_carrier -> UU :=
+| con_denotes : ∏ (a : pas_carrier), term_denotes_element (con a) a
+| app_denotes : ∏ (s t : terms_over_pas), ∏ (a b : pas_carrier),
+                let u := pas_app (a ,, b) in
+                ∏ (p : defined(u)),
+                term_denotes_element s a -> term_denotes_element t b ->
+                defined u ->
+                term_denotes_element (app s t) (value u p).
