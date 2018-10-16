634279bb0df110323bfe46febd9cef13e212f6b2
Tom de Jong <tdejong.ac@gmail.com>
2018-10-16 14:23:31 +0100

    Cleanup and consistent definition casing

--- UniMath/PartialityDominances/Partiality_and_Dominances.v
+++ UniMath/PartialityDominances/Partiality_and_Dominances.v
@@ -39,13 +39,13 @@ Proof.
   exact (f x).
 Defined.
 
-Definition isDis_D {X Y : UU} (f : X -> lift Y) : UU :=
+Definition is_disciplined {X Y : UU} (f : X -> lift Y) : UU :=
   ∃ (f' : X -> D_lift Y), tame f' = f.
 
-Definition Dis_D (X Y : UU) : UU :=
+Definition disciplined (X Y : UU) : UU :=
   ∑ f : X -> lift Y, isDis_D f.
 
-Definition Dis_D_carrier {X Y : UU} (m : Dis_D X Y) : X -> lift Y :=
+Definition disciplined_carrier {X Y : UU} (m : Dis_D X Y) : X -> lift Y :=
   pr1 m.
 
 End fix_a_D_and_prop_selection.
@@ -65,63 +65,3 @@ Definition structural_dominance_carrier (struc_dom : structural_dominance) :
 Definition structural_dominance_selection (struc_dom : structural_dominance) :
   selects_propositions (structural_dominance_carrier struc_dom) :=
   pr1 (pr2 struc_dom).
-
-Section fix_a_structural_dominance.
-
-Context (struc_dom : structural_dominance).
-
-Definition pas : UU :=
-  ∑ (A : UU), nonempty A ×
-  Dis_D (structural_dominance_carrier struc_dom)
-        (structural_dominance_selection struc_dom)
-        (A × A) A.
-
-Section fix_a_pas.
-
-Context (A : pas).
-
-Definition pas_carrier : UU := pr1 A.
-
-Definition pas_disciplined_map :
-  Dis_D (structural_dominance_carrier struc_dom)
-        (structural_dominance_selection struc_dom)
-        (pas_carrier × pas_carrier) (pas_carrier)
-  := pr2 (pr2 A).
-
-Definition pas_app : pas_carrier × pas_carrier -> lift(pas_carrier) := pr1 pas_disciplined_map.
-
-(* Terms over a pas *)
-
-Section fix_a_var_type.
-
-Context (X : Type).
-
-Inductive terms_over_pas : UU :=
-  | var : X -> terms_over_pas
-  | con : pas_carrier -> terms_over_pas
-  | app : terms_over_pas -> terms_over_pas -> terms_over_pas.
-
-Inductive term_denotes_element : terms_over_pas -> pas_carrier -> UU :=
-| con_denotes : ∏ (a : pas_carrier), term_denotes_element (con a) a
-| app_denotes : ∏ (s t : terms_over_pas), ∏ (a b : pas_carrier),
-                let u := pas_app (a ,, b) in
-                ∏ (p : defined(u)),
-                term_denotes_element s a -> term_denotes_element t b ->
-                defined u ->
-                term_denotes_element (app s t) (value u p).
-
-Definition term_denotes (t : terms_over_pas) : UU
-  := ∑ (a : pas_carrier), term_denotes_element t a.
-
-Delimit Scope pca with pca.
-Local Open Scope pca.
-
-(* TO DO: check level *)
-Notation "t ↓ a" := (term_denotes_element t a) (at level 50) : pca.
-Notation "t ↓" := (term_denotes t) (at level 50) : pca.
-
-Example constants_denote : ∏ (a : pas_carrier), con a ↓ a.
-Proof.
-  intro a.
-  exact (con_denotes a).
-Defined.
