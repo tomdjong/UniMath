b3799b02fcb025dfe91cec3b4d18b62e8db93bad
Tom de Jong <tdejong.ac@gmail.com>
2018-11-19 17:03:35 +0000

    Still working; but dcpo morphisms are not required by definition to preserve directed families

--- UniMath/Algebra/DCPO.v
+++ UniMath/Algebra/DCPO.v
@@ -21,7 +21,8 @@ Proof.
   use (pr2 (R (f i) u)). (* R is prop-valued. *)
 Qed.
 
-Definition islub (u : X) : UU := isupperbound u × ∏ (y : X), (∏ (i : I), R (f i) y) -> R u y.
+Definition islub (u : X) : UU := isupperbound u ×
+                                 ∏ (y : X), (∏ (i : I), R (f i) y) -> R u y.
 Lemma islub_isaprop (u : X) : isaprop(islub u).
 Proof.
   use isapropdirprod.
@@ -30,13 +31,14 @@ Proof.
     use (pr2 (R u x)). (* R is prop-valued. *)
 Qed.
 
-(* Definition directed : UU := ∏ (i j : I), ∑ (k : I), R (f i) (f k) × R (f j) (f k). *)
 Definition isdirected : UU := ∏ (i j : I), ∥∑ (k : I), R (f i) (f k) × R (f j) (f k)∥.
 Lemma isdirected_isaprop : isaprop isdirected.
 Proof.
   use impred. intro i. use impred. intro j.
   use isapropishinh.
 Qed.
+Definition directeduntruncated (i j : I) : UU :=
+  ∑ (k : I), R (f i) (f k) × R (f j) (f k).
 End FixIndexingFamily.
 End DirectedComplete.
 
@@ -60,18 +62,11 @@ Definition dcpo := ∑ (X : Poset), isdirectedcomplete X.
 Definition dcpoposet (D : dcpo) : Poset := pr1 D.
 Coercion dcpoposet : dcpo >-> Poset.
 Definition dcpocarrier (D : dcpo) : hSet := carrierofposet (dcpoposet D).
-Definition dcpoorder (D : dcpo) : PartialOrder (dcpocarrier D) := pr2 (dcpoposet D).
+Definition dcpoorder (D : dcpo) : PartialOrder (dcpocarrier D) :=
+  pr2 (dcpoposet D).
 Definition dcpowithleast := ∑ (D : dcpo), ∑ (l : dcpocarrier D), isleast l.
 
 Definition dcpopair (X : Poset) (i : isdirectedcomplete X) : dcpo := (X,,i).
-
-Definition preservesdirected {P Q : Poset} (f : P -> Q) : UU :=
-  ∏ (I : UU), ∏ (u : I -> P), isdirected u -> isdirected (f ∘ u).
-Lemma preservesdirected_isaprop {P Q : Poset} (f : P -> Q) : isaprop (preservesdirected f).
-Proof.
-  use impred; intro I. use impred; intro u.
-  use isapropimpl. use isdirected_isaprop.
-Qed.
 
 Definition preserveslub {P Q : Poset} (f : P -> Q) {I : UU} (u : I -> P) : UU :=
   ∏ (v : P), islub u v -> islub (f ∘ u) (f v).
@@ -83,26 +78,27 @@ Proof.
 Qed.
 
 Definition isdcpomorphism {D D' : dcpo} (f : D -> D') :=
-  preservesdirected f × ∏ (I : UU), ∏ (u : I -> D), isdirected u ->
-                                                    preserveslub f u.
-Lemma isdcpomorphism_isaprop {D D' : dcpo} (f : D -> D') : isaprop (isdcpomorphism f).
+  ∏ (I : UU), ∏ (u : I -> D), isdirected u -> preserveslub f u.
+Lemma isdcpomorphism_isaprop {D D' : dcpo} (f : D -> D') :
+  isaprop (isdcpomorphism f).
 Proof.
-  use isapropdirprod.
-  - use preservesdirected_isaprop.
-  - use impred; intro I; use impred; intro u. use isapropimpl. use preserveslub_isaprop.
+  use impred; intro I; use impred; intro u.
+  use isapropimpl. use preserveslub_isaprop.
 Qed.
 
 Definition dcpomorphism (D D' : dcpo) := ∑ (f : D -> D'), isdcpomorphism f.
-Definition dcpomorphismpair (D D': dcpo) : ∏ (t : D -> D'), isdcpomorphism t ->
-                                                            dcpomorphism D D'.
+Definition dcpomorphismpair (D D': dcpo) :
+  ∏ (t : D -> D'), isdcpomorphism t -> dcpomorphism D D'.
 Proof.
   intros t i. exact (t,,i).
 Defined.
 
-Definition dcpomorphismcarrier {D D' : dcpo} : dcpomorphism D D' -> (D -> D') := pr1.
+Definition dcpomorphismcarrier {D D' : dcpo} :
+  dcpomorphism D D' -> (D -> D') := pr1.
 Coercion dcpomorphismcarrier : dcpomorphism >-> Funclass.
 
-Lemma dcpomorphism_preservesorder {D D' : dcpo} (f : dcpomorphism D D') : isaposetmorphism f.
+Lemma dcpomorphism_preservesorder {D D' : dcpo} (f : dcpomorphism D D') :
+  isaposetmorphism f.
 Proof.
   intros x y ineq. set (two := coprod unit unit).
   set (fam := (λ t : two, match t with | inl _ => x | inr _ => y end)).
@@ -122,9 +118,18 @@ Proof.
       + simpl. use isrefl_posetRelation.
     - intros d hyp. exact (hyp (inr tt)). }
   assert (lubfam' : islub (f ∘ fam) (f y)).
-  { use (pr2 (pr2 f)). exact isdirec. exact lubfam. }
+  { use (pr2 f). exact isdirec. exact lubfam. }
   set (ineq' := pr1 lubfam'). use (ineq' (inl tt)).
 Qed.
+
+(* *** Redundant(?) ***
+
+Definition dcpomorphism_to_posetmorphism {D D' : dcpo} :
+  dcpomorphism D D' -> posetmorphism D D'.
+Proof.
+  intro f. split with f. use dcpomorphism_preservesorder.
+Defined.
+Coercion dcpomorphism_to_posetmorphism : dcpomorphism >-> posetmorphism.
 
 Definition posetmorphismorder (P Q : Poset) : hrel (posetmorphism P Q).
 Proof.
@@ -163,3 +168,54 @@ Proof.
     + use posetmorphismorder.
     + use posetmorphismorder_ispartialorder.
 Defined.
+*)
+
+Lemma lubpreservesorder {X : Poset} {I : UU} (u v : I -> X) :
+  (∏ (i : I), (posetRelation X) (u i) (v i)) ->
+  ∏ (lu lv : X), islub u lu -> islub v lv ->
+                 (posetRelation X) lu lv.
+Proof.
+  intros ineqs lu lv islubu islubv.
+  apply (pr2 islubu).
+  intro i. use istrans_posetRelation.
+  - exact (v i).
+  - exact (ineqs i).
+  - use (pr1 islubv).
+Qed.
+
+Definition posetdcpomorphisms {D D' : dcpo} : Poset.
+Proof.
+  use Posetpair.
+  - exact (dcpomorphism D D').
+  use Posetpair.
+
+
+
+(* DCPO of dcpo morphisms *)
+Definition dcpoofdcpomorphisms {D D' : dcpo} : dcpo.
+Proof.
+  use dcpopair.
+  - exact (posetofdcpomo D D').
+  - intros I F isdirec.
+    (* For any d : D, the family (f (d))_(f : F) is directed. *)
+    assert (isdirec' : ∏ (d : D), isdirected (λ i : I, (pr1 (F i)) d)).
+    { intros d i j. use factor_through_squash.
+      - exact (directeduntruncated F i j).
+      - use isapropishinh.
+      - intro direc. use hinhpr.
+        induction direc as [k ineqs].
+        split with k; simpl.
+        induction ineqs as [ineq1 ineq2]. split.
+        + exact (ineq1 d).
+        + exact (ineq2 d).
+      - exact (isdirec i j). }
+    (* The lub of (f : F) will be g *)
+    set (isdireccompl := pr2 D'); simpl in isdireccompl.
+    set (gdata := λ d : D, isdireccompl I _ (isdirec' d)).
+    set (g := λ d : D, pr1 (gdata d)).
+    assert (gpreservesorder : isaposetmorphism g).
+    { intros x y ineq. use lubpreservesorder.
+      - exact I.
+      - exact (λ i : I, pr1 (F i) x).
+      - exact (λ i : I, pr1 (F i) y).
+      - intro i. simpl.
