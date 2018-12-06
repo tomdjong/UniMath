d841d9a11ee00465b965392b9db53bf9b94280e7
Tom de Jong <tdejong.ac@gmail.com>
2018-12-06 12:43:32 +0000

    Added PCF to the file listing

--- UniMath/PartialityDominances/All.v
+++ UniMath/PartialityDominances/All.v
@@ -1,3 +1,4 @@
 (* This file has been auto-generated, do not edit it. *)
 Require Export UniMath.PartialityDominances.PartialElements.
 Require Export UniMath.PartialityDominances.PartialFunctions.
+Require Export UniMath.PartialityDominances.PCF.
