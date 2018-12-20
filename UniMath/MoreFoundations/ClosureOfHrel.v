e5936ad225f8e056a4af5b4fd1bdaf1adc01e53f
Tom de Jong <tdejong.ac@gmail.com>
2018-12-20 11:35:54 +0100

    Reflexive, transitive closure stepwise

--- UniMath/MoreFoundations/ClosureOfHrel.v
+++ UniMath/MoreFoundations/ClosureOfHrel.v
@@ -1,75 +1,5 @@
 Require Import UniMath.Foundations.All.
 
-Section reflexive_closure_hrel.
-  Context {X : UU}.
-
-Inductive refl_clos (R : hrel X) : X -> X -> UU :=
-  | base_step' (x y : X) : R x y -> refl_clos R x y
-  | refl_step' (x : X) : refl_clos R x x.
-
-Delimit Scope reflclos with reflclos.
-Local Open Scope reflclos.
-
-Context (R : hrel X).
-Notation "'R''" := (refl_clos R) : reflclos.
-
-Lemma refl_clos_extends : ∏ (x y : X), R x y -> R' x y.
-Proof.
-  use base_step'.
-Qed.
-
-Lemma refl_clos_refl : ∏ (x : X), R' x x.
-Proof.
-  use refl_step'.
-Qed.
-
-Lemma refl_clos_univprop : ∏ (S : X -> X -> UU),
-                           (∏ (x y : X), R x y -> S x y) ->
-                           (∏ (x : X), S x x) ->
-                           ∏ (x y : X), R' x y -> S x y.
-Proof.
-  intros S extends refl x y. intro hyp.
-  induction hyp.
-  - use extends. exact h.
-  - use refl.
-Qed.
-
-Definition refl_clos_hrel (x y : X) := ∥ R' x y ∥.
-
-Notation "'R*'" := (refl_clos_hrel) : reflclos.
-
-Lemma refl_clos_hrel_ishrel : ∏ (x y : X), isaprop (R* x y).
-Proof.
-  intros x y. use isapropishinh.
-Qed.
-
-Lemma refl_clos_hrel_extends : ∏ (x y : X), R x y -> R* x y.
-Proof.
-  intros x y R1. use hinhpr. use refl_clos_extends. exact R1.
-Qed.
-
-Lemma refl_clos_hrel_isrefl : isrefl R*.
-Proof.
-  intro x. use hinhpr. use refl_clos_refl.
-Qed.
-
-Lemma refl_clos_hrel_univprop : ∏ (S : hrel X),
-                                      (∏ (x y : X), R x y -> S x y) ->
-                                      (∏ (x : X), S x x) ->
-                                      ∏ (x y : X), R* x y -> S x y.
-Proof.
-  intros S extends refl x y. intro hyp.
-  use factor_through_squash.
-  - exact (R' x y).
-  - use (pr2 (S x y)).
-  - use (refl_clos_univprop (λ x y : X, (pr1 (S x y)))).
-    + use extends.
-    + use refl.
-  - exact hyp.
-Qed.
-
-End reflexive_closure_hrel.
-
 Section reflexive_transitive_closure_hrel.
   Context {X : UU}.
 
@@ -169,5 +99,86 @@ Proof.
   - exact hyp.
 Qed.
 
-
 End reflexive_transitive_closure_hrel.
+
+Section reflexive_transitive_closure_step_hrel.
+  Context {X : UU}.
+
+Inductive refl_trans_clos_step (R : hrel X) : nat -> X -> X -> UU :=
+  | base_step' (x y : X)              : R x y -> refl_trans_clos_step R 0 x y
+  | refl_step' (x : X)                : refl_trans_clos_step R 0 x x
+  | trans_step' (x y z : X) (n : nat) : refl_trans_clos_step R n x y -> R y z ->
+                                        refl_trans_clos_step R (S n) x z.
+
+Inductive refl_trans_clos' (R : hrel X) : X -> X -> UU :=
+  | refl_step'' (x : X) : refl_trans_clos' R x x
+  | trans_step'' (x y z : X) : refl_trans_clos' R x y -> R y z ->
+                               refl_trans_clos' R x z.
+
+Delimit Scope refltransclos' with refltransclos'.
+Local Open Scope refltransclos'.
+
+Context (R : hrel X).
+Notation "'R''" := (refl_trans_clos' R) : refltransclos'.
+
+Lemma refl_trans_clos'_extends : ∏ (x y : X), R x y -> R' x y.
+Proof.
+  intros x y. apply trans_step''.
+  use refl_step''.
+Qed.
+
+Lemma refl_trans_clos'_refl : ∏ (x : X), R' x x.
+Proof.
+  use refl_step''.
+Qed.
+
+Lemma refl_trans_clos'_trans : ∏ (x y z : X), R' x y -> R' y z -> R' x z.
+Proof.
+  intros x y z R1 R2.
+  induction R2.
+  - exact R1.
+  - eapply trans_step''.
+    + apply IHR2.
+      exact R1.
+    + exact h.
+Qed.
+
+End reflexive_transitive_closure_step_hrel.
+
+Definition refl_trans_clos_equiv {X : UU} (R : hrel X) (x y : X) :
+  refl_trans_clos R x y <-> refl_trans_clos' R x y.
+Proof.
+  split.
+  - use refl_trans_clos_univprop.
+    + use refl_trans_clos'_extends.
+    + use refl_trans_clos'_refl.
+    + use refl_trans_clos'_trans.
+  - intro left. induction left.
+    + use refl_step.
+    + eapply trans_step.
+      ++ exact IHleft.
+      ++ apply base_step.
+         exact h.
+Defined.
+
+Definition refl_trans_clos'_approx {X : UU} (R : hrel X) (x y : X) :
+  refl_trans_clos' R x y <-> ∑ (k : nat), refl_trans_clos_step R k x y.
+Proof.
+  split.
+  - intro left.
+    induction left.
+    + split with 0. use refl_step'.
+    + induction IHleft as [m rel].
+      split with (S m).
+      eapply trans_step'.
+      ++ exact rel.
+      ++ exact h.
+  - intros [k left].
+    induction left.
+    + use refl_trans_clos'_extends.
+      exact h.
+    + use refl_trans_clos'_refl.
+    + eapply trans_step''.
+      ++ exact IHleft.
+      ++ exact h.
+Defined.
