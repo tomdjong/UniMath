f544186d8898d1ff9532913961c21e54373e4367
Tom de Jong <tdejong.ac@gmail.com>
2019-01-15 12:39:57 +0000

    Decidable W-types

--- UniMath/PartialityDominances/test.v
+++ UniMath/PartialityDominances/test.v
@@ -322,3 +322,89 @@ Proof.
   induction e.
   apply idpath.
 Defined.
+
+Inductive Wtype {A : UU} {B : A -> UU} :=
+| sup (a : A) (f : B a -> Wtype) : Wtype.
+
+Context {A : UU}.
+Context {B : A -> UU}.
+Context (Adec : isdeceq A).
+
+Fixpoint Wtype_code (u v : Wtype) : UU :=
+  match u with
+  | sup a f =>
+    match v with
+    | sup a' f' =>
+      match (Adec a a') with
+      | inr _ => empty
+      | inl e => ∏ (b : B a), Wtype_code (f b) (f' (transportf B e b))
+      end
+    end
+  end.
+
+Definition Wtype_r (w : Wtype) : Wtype_code w w.
+Proof.
+  induction w as [a f c].
+  simpl.
+  induction (Adec a a) as [eq | neq].
+  - assert (triveq : eq = idpath _).
+    { apply proofirrelevance, isasetifdeceq, Adec. }
+    rewrite triveq. apply c.
+  - apply neq, idpath.
+Defined.
+
+Definition Wtype_encode (u v : Wtype) : u = v -> Wtype_code u v :=
+  λ (p : u = v), transportf (λ w : Wtype, Wtype_code u w) p (Wtype_r u).
+
+Context (B_Pidec : ∏ (a : A) (C : B a -> UU), (∏ (b : B a), decidable (C b)) ->
+                                              decidable (∏ (b : B a), C b)).
+
+Definition Wtype_decode : ∏ (u v : Wtype),  Wtype_code u v -> u = v.
+Proof.
+  induction u as [a f IH].
+  induction v as [a' f' _].
+  unfold Wtype_code.
+  induction (Adec a a') as [eq | neq].
+  - intro c.
+    assert (feq : ∏ (b : B a), f b = f' (transportf B eq b)).
+    { intro b. apply (IH b (f' (transportf B eq b))).
+      apply (c b). }
+    set (intermed := sup a (f' ∘ transportf B eq)).
+    intermediate_path intermed.
+    + unfold intermed. apply maponpaths.
+      apply funextfun.
+      exact feq.
+    + unfold intermed. generalize eq as e.
+      induction e. apply idpath.
+  - apply fromempty.
+Defined.
+
+Definition decidable_iff (X Y : UU) (iff : X <-> Y) : decidable X <-> decidable Y.
+Proof.
+  induction iff as [f g].
+  split.
+  - intro Xdec. induction Xdec as [x | nx].
+    + apply inl. exact (f x).
+    + apply inr. intro y. apply nx. exact (g y).
+  - intro Ydec. induction Ydec as [y | ny].
+    + apply inl. exact (g y).
+    + apply inr. intro x. apply ny. exact (f x).
+Defined.
+
+Definition Wtype_dec : isdeceq (@Wtype A B).
+Proof.
+  unfold isdeceq. intros u' v'.
+  eapply decidable_iff.
+  - split.
+    + apply Wtype_decode.
+    + apply Wtype_encode.
+  - generalize v' as v. generalize u' as u.
+    clear u' v'.
+    induction u as [a f IH].
+    induction v as [a' f' _].
+    unfold Wtype_code.
+    induction (Adec a a') as [eq | neq].
+    + apply (B_Pidec a _). intro b.
+      exact (IH b (f' (transportf B eq b))).
+    + apply isdecpropempty.
+Defined.
