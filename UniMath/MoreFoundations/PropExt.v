c34f337d697fcfe610639025c83eff65824a3311
Tom de Jong <tdejong.ac@gmail.com>
2019-01-23 17:53:42 +0000

    Identity type of props is a prop, without univalence

--- UniMath/MoreFoundations/PropExt.v
+++ UniMath/MoreFoundations/PropExt.v
@@ -1,4 +1,5 @@
 Require Import UniMath.Foundations.All.
+Require Import UniMath.MoreFoundations.WeaklyConstant.
 
 Definition propext {X Y : UU} : isaprop X -> isaprop Y -> (X <-> Y) -> X = Y.
 Proof.
@@ -7,5 +8,32 @@ Proof.
   use weqiff.
   - exact iff.
   - exact i.
+  - exact j.
+Defined.
+
+Definition weaklyconstant_propext {X Y : UU} (i : isaprop X) (j : isaprop Y) :
+  weaklyconstant (propext i j).
+Proof.
+  intros f g.
+  apply maponpaths.
+  apply proofirrelevance.
+  apply isapropdirprod.
+  - apply impred_isaprop.
+    exact (λ _, j).
+  - apply impred_isaprop.
+    exact (λ _, i).
+Defined.
+
+Definition isaprop_pathsprop {X Y : UU} : isaprop X -> isaprop Y -> isaprop (X = Y).
+Proof.
+  intros i j.
+  apply wconst_endomap_prop_path_prop.
+  - exact i.
+  - intros Z k.
+    set (f := λ p : X = Z, (propext i k (weq_to_iff (eqweqmap p)))).
+    exists f.
+    unfold f.
+    intros p q.
+    apply weaklyconstant_propext.
   - exact j.
 Defined.
