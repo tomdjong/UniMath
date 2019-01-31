43b3eb7eac8a84c089f172692c0ef934236421a9
Tom de Jong <tdejong.ac@gmail.com>
2019-01-31 16:10:59 +0000

    Cleanup PropExt

--- UniMath/MoreFoundations/PropExt.v
+++ UniMath/MoreFoundations/PropExt.v
@@ -1,11 +1,25 @@
+(**
+
+Tom de Jong
+
+January 2019
+
+*******************************************************************************)
+
+(** * Propositional extensionality *)
+(** ** Contents
+- A proof using just propositional extensionality of the fact that:
+  if P and Q are propositions, then so is P = Q.
+*)
+
 Require Import UniMath.Foundations.All.
 Require Import UniMath.MoreFoundations.WeaklyConstant.
 
 Definition propext {X Y : UU} : isaprop X -> isaprop Y -> (X <-> Y) -> X = Y.
 Proof.
   intros i j iff.
-  use (invmap (univalence X Y)).
-  use weqiff.
+  apply (invmap (univalence X Y)).
+  apply weqiff.
   - exact iff.
   - exact i.
   - exact j.
